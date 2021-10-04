472608e4708b3fdd5d314bd6540f0f1ee7b0f090
Brian Holt <bholtbholt@icloud.com>
2021-10-03 17:28:41 -0700

    Only init headroom on desktop

--- js/headroom-init.js
+++ js/headroom-init.js
@@ -1,5 +1,9 @@
 (() => {
-  const myElement = document.querySelector('[data-behavior~="headroom"]');
-  const headroom = new Headroom(myElement);
-  headroom.init();
+  const mediaQuery = window.matchMedia('(min-width: 768px)');
+  // Only init headroom on desktop and above
+  if (mediaQuery.matches) {
+    const myElement = document.querySelector('[data-behavior~="headroom"]');
+    const headroom = new Headroom(myElement);
+    headroom.init();
+  }
 })();
